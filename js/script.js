985e9f34fe2376d9a5d4e697c793fbc0d323c3dc
MariaScheib <Maria@mariascheibe.com>
2016-04-02 10:40:40 -0400

    final project

---
+++ js/script.js
@@ -0,0 +1,43 @@
+console.log ("HELLO!")
+
+// // $('.Puppets').click(function()){
+// // 	$('.Puppets').css('opacity',0);
+// // 	$('.2d Work').css('Opacity',1);
+// // }
+
+// // $('.Puppets').ready(function(){
+// //     $("button").click(function(){
+// //         $("p").click();
+// //     });
+// // });
+
+// // $('.Puppets').click(function (e) {
+// //     $('#home_splash2').fadeOut('slow', function () {
+// //         $('#home_splash1').fadeIn('slow');
+// //     });
+// // });
+
+
+// <!DOCTYPE html>
+// <head>
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+// <script>
+// $(document).ready(function(){
+//     $(".btn1").click(function(){
+//         $("p").fadeOut()
+//     });
+//     $(".btn2").click(function(){
+//         $("p").fadeIn();
+//     });
+// });
+// </script>
+// </head>
+// <body>
+
+// <p>This is a paragraph.</p>
+
+// <button class="btn1">Fade out</button>
+// <button class="btn2">Fade in</button>
+
+// </body>
+// </html>
